755808ca5e42153c4839e24a745ffa5817b62dd5
Suresh Suthar <158549605+suthar-suresh@users.noreply.github.com>
2025-05-08 12:01:06 +0000

    Refactor AdminLogsPage and API route for improved data handling and structure

--- src/app/admin/logs/page.tsx
+++ src/app/admin/logs/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 interface Language {
   name: string;
 }
 
-interface IPLog {
+interface location {
+  languages: Language[];
+  country_flag: string;
+}
+
+interface fullResponse {
   ip: string;
   continent_name: string;
   country_name: string;
@@ -14,13 +20,17 @@ interface IPLog {
   city: string;
   latitude: number;
   longitude: number;
-  languages: Language[];
-  date: string;
-  time: string;
+  location: location;
+  zip: string;
 }
 
+interface IPLog {
+  fullResponse: fullResponse;
+  timestamp: string;
+}
 export default function AdminLogsPage() {
   const [logs, setLogs] = useState<IPLog[]>([]);
+  const [country_names, setCountry_names] = useState<[String]>(['']);
   const [queryKey, setQueryKey] = useState("");
   const [accessGranted, setAccessGranted] = useState(false);
   const [page, setPage] = useState(1);
@@ -28,10 +38,15 @@ export default function AdminLogsPage() {
   const [totalLogs, setTotalLogs] = useState(0);
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
+  const [selectedCountry, setSelectedCountry] = useState("");
+  const [selectedRegion, setSelectedRegion] = useState("");
+  const [selectedCity, setSelectedCity] = useState("");
+  const [Region, setRegion] = useState<[String]>(['']);
+  const [City, setCity] = useState<[String]>(['']);
 
-  const fetchLogs = async (pageNum = 1) => {
+  const fetchLogs = async (event: React.FormEvent, pageNum = 1) => {
+    event.preventDefault();
     const params = new URLSearchParams({
-      key: queryKey,
       page: pageNum.toString(),
       limit: "10",
     });
@@ -42,57 +57,101 @@ export default function AdminLogsPage() {
     if (res.ok) {
       const data = await res.json();
       setLogs(data.logs);
+      console.log(data);
       setTotalPages(data.totalPages);
       setTotalLogs(data.totalLogs);
       setPage(data.page);
       setAccessGranted(true);
+      setCountry_names(data.country_name);
+      setRegion(data.regions_name);
+      setCity(data.cities_name);
     } else {
       alert("Access denied");
     }
   };
-
+console.log(selectedCity, selectedRegion, selectedCountry);
   return (
     <div className="p-6 pt-20 max-w-screen-xl mx-auto text-white">
       {!accessGranted ? (
         <div className="max-w-md mx-auto">
           <h2 className="text-xl font-bold mb-4">Enter Admin Key</h2>
-          <input
-            type="password"
-            className="border p-2 w-full rounded mb-2 bg-gray-800 text-white"
-            value={queryKey}
-            onChange={(e) => setQueryKey(e.target.value)}
-            placeholder="Admin key"
-          />
-          <button
-            onClick={() => fetchLogs()}
-            className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
-          >
-            Submit
-          </button>
+          <form action="">
+            <input
+              type="password"
+              className="border p-2 w-full rounded mb-2 bg-gray-800 text-white"
+              value={queryKey}
+              onChange={(e) => setQueryKey(e.target.value)}
+              placeholder="Admin key"
+            />
+            <button
+              type="submit"
+              onClick={(e) => fetchLogs(e as React.FormEvent)}
+              className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
+            >
+              Submit
+            </button>
+          </form>
         </div>
       ) : (
         <>
           <div className="mb-4 flex flex-wrap gap-4 items-center">
-            <h2 className="text-2xl font-bold">IP Logs</h2>
-            <input
-              type="date"
-              value={startDate}
-              onChange={(e) => setStartDate(e.target.value)}
-              className="border p-2 rounded bg-gray-800 text-white"
-            />
-            <input
-              type="date"
-              value={endDate}
-              onChange={(e) => setEndDate(e.target.value)}
-              className="border p-2 rounded bg-gray-800 text-white"
-            />
-            <button
-              onClick={() => fetchLogs(1)}
-              className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
-            >
-              Filter
-            </button>
-          </div>
+  <h2 className="text-2xl font-bold">IP Logs</h2>
+  
+  <input
+    type="date"
+    value={startDate}
+    onChange={(e) => setStartDate(e.target.value)}
+    className="border p-2 rounded bg-gray-800 text-white"
+  />
+  
+  <input
+    type="date"
+    value={endDate}
+    onChange={(e) => setEndDate(e.target.value)}
+    className="border p-2 rounded bg-gray-800 text-white"
+  />
+
+  <select
+    value={selectedCountry}
+    onChange={(e) => setSelectedCountry(e.target.value)}
+    className="border p-2 rounded bg-gray-800 text-white"
+    >
+    <option value="">Select Country</option>
+    {country_names.map((country_names)=>(<>
+    <option value="USA">{country_names}</option></>
+  ))}
+  </select>
+
+  <select
+    value={selectedRegion}
+    onChange={(e) => setSelectedRegion(e.target.value)}
+    className="border p-2 rounded bg-gray-800 text-white"
+    >
+    <option value="">Select Region</option>
+    {Region.map((region)=>(<>
+    <option value="USA">{region}</option></>
+  ))}
+  </select>
+
+  <select
+    value={selectedCity}
+    onChange={(e) => setSelectedCity(e.target.value)}
+    className="border p-2 rounded bg-gray-800 text-white"
+    >
+    <option value="">Select City</option>
+    {City.map((city)=>(<>
+    <option value="USA">{city}</option></>
+  ))}
+  </select>
+
+  <button
+    onClick={(e) => fetchLogs(e, 1)}
+    className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
+  >
+    Filter
+  </button>
+</div>
+
 
           <div className="mb-2 text-sm text-gray-300">
             Showing <strong>{logs.length}</strong> of <strong>{totalLogs}</strong> logs
@@ -116,20 +175,35 @@ export default function AdminLogsPage() {
               </thead>
               <tbody>
                 {logs.map((log, i) => {
+                  const fullResponse = log.fullResponse || {};
+                  const date = new Date(log.timestamp);
+                  const formattedDate = date.toLocaleDateString(); // e.g., "5/8/2025"
+                  const formattedTime = date.toLocaleTimeString();
                   return (
                     <tr key={i} className="odd:bg-gray-800 even:bg-gray-700 hover:bg-gray-600">
-                      <td className="border px-3 py-2">{log.ip}</td>
-                      <td className="border px-3 py-2">{log.continent_name}</td>
-                      <td className="border px-3 py-2">{log.country_name}</td>
-                      <td className="border px-3 py-2">{log.region_name}</td>
-                      <td className="border px-3 py-2">{log.city}</td>
-                      <td className="border px-3 py-2">{log.latitude}</td>
-                      <td className="border px-3 py-2">{log.longitude}</td>
+                      <td className="border px-3 py-2">{fullResponse.ip}</td>
+                      <td className="border px-3 py-2">{fullResponse.continent_name}</td>
                       <td className="border px-3 py-2">
-                        {log.languages.map((l) => l.name).join(", ")}
+                        <div className="flex items-center gap-2">
+                          <Image
+                            src={fullResponse.location.country_flag || ""}
+                            width={20}
+                            height={20}
+                            alt={fullResponse.continent_name}
+                          />
+                          <span>{fullResponse.country_name}</span>
+                        </div>
                       </td>
-                      <td className="border px-3 py-2">{log.date}</td>
-                      <td className="border px-3 py-2">{log.time}</td>
+
+                      <td className="border px-3 py-2">{fullResponse.region_name}</td>
+                      <td className="border px-3 py-2">{fullResponse.city}</td>
+                      <td className="border px-3 py-2">{fullResponse.latitude}</td>
+                      <td className="border px-3 py-2">{fullResponse.longitude}</td>
+                      <td className="border px-3 py-2">
+                        {fullResponse.location.languages[0].name || "-"}
+                      </td>
+                      <td className="border px-3 py-2">{formattedDate}</td>
+                      <td className="border px-3 py-2">{formattedTime}</td>
                     </tr>
                   );
                 })}
@@ -139,11 +213,10 @@ export default function AdminLogsPage() {
 
           <div className="flex justify-between mt-4">
             <button
-              onClick={() => fetchLogs(page - 1)}
+              onClick={(e) => fetchLogs(e as React.FormEvent, page - 1)}
               disabled={page <= 1}
-              className={`px-4 py-2 rounded ${
-                page <= 1 ? "bg-gray-500 text-gray-300" : "bg-blue-600 text-white hover:bg-blue-700"
-              }`}
+              className={`px-4 py-2 rounded ${page <= 1 ? "bg-gray-500 text-gray-300" : "bg-blue-600 text-white hover:bg-blue-700"
+                }`}
             >
               Previous
             </button>
@@ -151,13 +224,12 @@ export default function AdminLogsPage() {
               Page <strong>{page}</strong> of <strong>{totalPages}</strong>
             </span>
             <button
-              onClick={() => fetchLogs(page + 1)}
+              onClick={(e) => fetchLogs(e as React.FormEvent, page + 1)}
               disabled={page >= totalPages}
-              className={`px-4 py-2 rounded ${
-                page >= totalPages
-                  ? "bg-gray-500 text-gray-300"
-                  : "bg-blue-600 text-white hover:bg-blue-700"
-              }`}
+              className={`px-4 py-2 rounded ${page >= totalPages
+                ? "bg-gray-500 text-gray-300"
+                : "bg-blue-600 text-white hover:bg-blue-700"
+                }`}
             >
               Next
             </button>
--- src/app/api/admin/logs/route.ts
+++ src/app/api/admin/logs/route.ts
@@ -9,12 +9,12 @@ export async function GET(request: NextRequest) {
     const start = request.nextUrl.searchParams.get("start");
     const end = request.nextUrl.searchParams.get("end");
 
-    if (key !== process.env.ADMIN_QUERY_KEY) {
-      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-    }
-
     const db = await connectToDatabase();
     const ipapiCollection = db.collection("ipapi_responses");
+  
+  const country_name = await ipapiCollection.distinct("fullResponse.country_name")
+  const regions_name = await ipapiCollection.distinct("fullResponse.region_name");
+  const cities_name = await ipapiCollection.distinct("fullResponse.city");
 
     const query: any = {};
     if (start || end) {
@@ -27,30 +27,23 @@ export async function GET(request: NextRequest) {
     const total = await ipapiCollection.countDocuments(query);
     const totalPages = Math.max(Math.ceil(total / limit), 1);
 
-    // Debug: Check the calculated totalPages and other pagination values
-    console.log("Total logs:", total);
-    console.log("Total pages:", totalPages);
-    console.log("Requested page:", page);
-
     const currentPage = Math.min(Math.max(page, 1), totalPages);
     const skip = (currentPage - 1) * limit;
-
-    // Debug: Log skip value and pagination values
-    console.log("Skip:", skip);
-    console.log("Limit:", limit);
 
     const logs = await ipapiCollection
       .find(query, {
         projection: {
           timestamp: 1,
           "fullResponse.ip": 1,
+          "fullResponse.zip": 1,
           "fullResponse.continent_name": 1,
           "fullResponse.country_name": 1,
           "fullResponse.region_name": 1,
           "fullResponse.city": 1,
           "fullResponse.latitude": 1,
           "fullResponse.longitude": 1,
-          "fullResponse.languages.name": 1,
+         "fullResponse.location.languages": 1,
+          "fullResponse.location.country_flag": 1,
         },
       })
       .sort({ timestamp: -1 })
@@ -58,30 +51,14 @@ export async function GET(request: NextRequest) {
       .limit(limit)
       .toArray();
 
-    const formattedLogs = logs.map((doc) => {
-      const timestamp = doc.timestamp ? new Date(doc.timestamp) : null;
-      const date = timestamp ? timestamp.toISOString().split("T")[0] : "-";
-      const time = timestamp ? timestamp.toTimeString().split(" ")[0] : "-";
-
-      return {
-        ip: doc.fullResponse?.ip || "-",
-        continent_name: doc.fullResponse?.continent_name || "-",
-        country_name: doc.fullResponse?.country_name || "-",
-        region_name: doc.fullResponse?.region_name || "-",
-        city: doc.fullResponse?.city || "-",
-        latitude: doc.fullResponse?.latitude || "-",
-        longitude: doc.fullResponse?.longitude || "-",
-        languages: doc.fullResponse?.languages || [],
-        date,
-        time,
-      };
-    });
-
     return NextResponse.json({
-      logs: formattedLogs,
+      logs: logs,
       page: currentPage,
       totalPages,
       totalLogs: total,
+      country_name: country_name,
+      regions_name: regions_name,
+      cities_name: cities_name,
     });
   } catch (error) {
     console.error("API Error:", error);
